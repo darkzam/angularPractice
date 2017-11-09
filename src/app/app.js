9e1cb5ee24e584fedcb6a67b37ccef89220def6f
zamir narvaez <zamir.narvaez@correounivalle.edu.co>
2017-11-09 17:18:51 -0500

    ex 1

---
+++ src/app/app.js
@@ -0,0 +1,9 @@
+var container = document.getElementById('container');
+function Counter(myElement) {
+    this.count = 0;
+    myElement.innerHTML = this.count;
+    myElement.addEventListener('click', function () {
+        myElement.innerHTML = +(myElement.innerHTML) + 1;
+    });
+}
+new Counter(container);
